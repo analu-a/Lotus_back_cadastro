db7370940971e56c18635e936e661ca49b066147
Ana Luiza <analuiza.azevedomorais@gmail.com>
2024-09-05 11:33:58 -0300

    banco

---
+++ sql_cms.sql
@@ -0,0 +1,21 @@
+create database cms;
+use cms;
+
+create table cadastro
+(
+id_cadastro int not null auto_increment primary key ,
+nome varchar(60) not null,
+sobrenome varchar(60) not null,
+email varchar(60) not null,
+senha varchar(60)not null
+);
+
+insert into cadastro (nome, sobrenome, email, senha) values
+
+("Fabio",  "alexandre",  "fabiosk8@gmail.com",  "abc123"),
+("ale",  "alexandre", "ale@gmail.com",  "abc124"),
+("lucicleide",  "alexandre", "lucicleide@gmail.com",  "abc125"),
+("lukinhas",  "alexandre", "mcbrinquedohaha@gmail.com", "abc126"),
+("pato",  "alexandre", "patodonalds@gmail.com", "abc127");
+
+drop database cms;
